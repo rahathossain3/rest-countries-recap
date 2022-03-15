2c4496ae04f7b0890e8f77b4b621b32ca5627d6d
rahathossain3 <rahathossain210@gmail.com>
2022-03-15 12:51:16 +0600

    first commit

---
+++ js/countries.js
@@ -0,0 +1,25 @@
+const loadCountries = () => {
+    const url = `https://restcountries.com/v3.1/all`;
+    fetch(url)
+        .then(responce => responce.json())
+        .then(data => displayCountries(data))
+    // .catch(err => console.log(err));
+}
+
+const displayCountries = countries => {
+    console.log(countries);
+    const countryHTML = countries.map(country => getContryElement(country));
+
+    console.log(countryHTML)
+}
+
+const getContryElement = country => {
+    return `
+    <div>
+    <h2> ${country.name.common} </h2>
+    </div>
+
+    `
+}
+
+loadCountries();
