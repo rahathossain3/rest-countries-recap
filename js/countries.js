f94d813f599e02b3ca12986854c13571a5da1824
rahathossain3 <rahathossain210@gmail.com>
2022-03-15 17:32:23 +0600

    props

--- js/countries.js
+++ js/countries.js
@@ -7,8 +7,8 @@ const loadCountries = () => {
 }
 
 const displayCountries = countries => {
-    console.log(countries[0].flags);
-    const countryHTML = countries.map(country => getContryElement(country));
+    console.log(countries[0]);
+    const countryHTML = countries.map(country => getCountryElement(country));
     const container = document.getElementById('countries');
     container.innerHTML = countryHTML.join('');
 
@@ -17,14 +17,50 @@ const displayCountries = countries => {
 
 // Array.join('ja diye join kore ischuk')
 
-const getContryElement = country => {
+
+//option 2------------
+const getCountryElement = ({ name, flags, area, region }) => {
+    //parameter er modde destructuring 
     return `
     <div class="country">
-    <h2> ${country.name.common} </h2>
-    <img src="${country.flags.png}">
+    <h2> ${name.common} </h2>
+    <p>Area: ${area}</p>
+    <p>Continent: ${region}</p>
+    <img src="${flags.png}">
     </div>
 
-    `
+    `;
 }
+
+
+
+
+//  Option 1------------
+// const getCountryElement = country => {
+//     //option 1 destructuring
+//     const { name, flags } = country;
+//     return `
+//     <div class="country">
+//     <h2> ${name.common} </h2>
+//     <img src="${flags.png}">
+//     </div>
+
+//     `;
+// } 
+// 
+
+
+//original --------
+// const getCountryElement = country => {
+//     return `
+//     <div class="country">
+//     <h2> ${country.name.common} </h2>
+//     <img src="${country.flags.png}">
+//     </div>
+
+//     `;
+// }
+
+
 
 loadCountries();
