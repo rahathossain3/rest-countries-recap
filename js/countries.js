9f9c260a1fea46364218ee3e9d59e74a05839449
rahathossain3 <rahathossain210@gmail.com>
2022-03-15 14:36:23 +0600

    done

--- js/countries.js
+++ js/countries.js
@@ -7,19 +7,21 @@ const loadCountries = () => {
 }
 
 const displayCountries = countries => {
-    console.log(countries);
+    console.log(countries[0].flags);
     const countryHTML = countries.map(country => getContryElement(country));
     const container = document.getElementById('countries');
-    container.innerHTML = countryHTML;
-
+    container.innerHTML = countryHTML.join('');
 
     // console.log(countryHTML)
 }
 
+// Array.join('ja diye join kore ischuk')
+
 const getContryElement = country => {
     return `
-    <div>
+    <div class="country">
     <h2> ${country.name.common} </h2>
+    <img src="${country.flags.png}">
     </div>
 
     `
